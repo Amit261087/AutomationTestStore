845a9e304e6639b477a4a47f118956b1792a3287
Amit261087 <amit.sharmaamit.sharma692@gmail.com>
2023-07-11 23:35:29 +0530

    test

--- cypress/e2e/AddProductInCart.cy.js
+++ cypress/e2e/AddProductInCart.cy.js
@@ -1,41 +1,51 @@
 let loginName;
 let password;
+
 before(() => {
-    cy.fixture('userCredentials.json').then((credential) => {
-        loginName = Cypress.env('loginName', credential.loginName)
-        password = Cypress.env('password', credential.password)
-    })
-})
+  cy.fixture('userCredentials.json').then((credential) => {
+    loginName = Cypress.env('loginName', credential.loginName);
+    password = Cypress.env('password', credential.password);
+  });
+});
+
 describe('Add Product in Cart', function () {
-    it('Add Product in Cart', () => {
-        cy.visit('https://automationteststore.com')
-        cy.contains('Login or register')
-            .should('be.visible')
-            .click()
-        cy.get('#loginFrm_loginname')
-            .should('be.visible')
-            .type(loginName)
-        cy.get('#loginFrm_password')
-            .should('be.visible')
-            .type(password)
-        cy.get("button[title='Login']")
-            .should('be.visible')
-            .click()
-        cy.get('.maintext')
-            .should('be.visible')
-            .and('contain', 'My Account')
-        cy.get('.active.menu_home')
-            .should('be.visible')
-            .and('have.text', 'Home')
-            .focus()
-            .click()
-        cy.get("a[class$='prdocutname']").each(($el, index, $list) => {
-            const text = $el.text()
-            if (text == 'Skinsheen Bronzer Stick') {
-                cy.get('.productcart').then($link => {
-                    cy.wrap($link).click()
-                })
-            }
-        })
+  it('Add Product in Cart', () => {
+    cy.visit('https://automationteststore.com');
+    cy.contains('Login or register')
+      .should('be.visible')
+      .click();
+    cy.get('#loginFrm_loginname')
+      .should('be.visible')
+      .type(loginName);
+    cy.get('#loginFrm_password')
+      .should('be.visible')
+      .type(password);
+    cy.get("button[title='Login']")
+      .should('be.visible')
+      .click();
+    cy.get('.maintext')
+      .should('be.visible')
+      .and('contain', 'My Account');
+    cy.get('.active.menu_home')
+      .should('be.visible')
+      .and('have.text', 'Home')
+      .focus()
+      .click();
+
+    cy.get("a[class$='prdocutname']").each(($el, index, $list) => {
+      const text = $el.text();
+      if (text === 'New Ladies High Wedge Heel Toe Thong Diamante Flip Flop Sandals') {
+        cy.get('.nostock')
+          .its('length')
+          .then((count) => {
+            cy.log(`Total out-of-stock products: ${count}`);
+            const adjustedIndex = index - count;
+            cy.get('.productcart')
+              .eq(adjustedIndex)
+              .click();
+          });
+        return false; // Exit the loop after clicking the desired element
+      }
     });
+  });
 });
