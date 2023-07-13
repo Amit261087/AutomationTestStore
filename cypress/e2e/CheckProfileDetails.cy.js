ad5b01a56005277dc135bf0fabaa4f93b29c85b2
Amit261087 <amit.sharmaamit.sharma692@gmail.com>
2023-07-13 23:44:29 +0530

    test

---
+++ cypress/e2e/CheckProfileDetails.cy.js
@@ -0,0 +1,27 @@
+let loginName;
+let password;
+
+before(()=>{
+    cy.fixture('userCredentials.json').then((credential)=>{
+        loginName = Cypress.env('loginName', credential.loginName)
+        password = Cypress.env('password', credential.password)
+    })
+})
+
+describe('Check Profile Details', function(){
+
+    it('Check Profile Details', function(){
+        cy.visit('https://automationteststore.com/')
+        cy.contains('Login or register').click()
+        cy.get('.returncustomer').should('be.visible').and('contain', 'Returning Customer')
+        cy.get('#loginFrm_loginname').should('be.visible').type(loginName)
+        cy.get('#loginFrm_password').should('be.visible').type(password)
+        cy.get("button[title='Login']").should('be.visible').click()
+        cy.get("div[class='menu_text']").should('be.visible').and('have.text', 'Welcome back Amit')
+        cy.get("div[class='menu_text']").trigger('mouseover').then(()=>{
+            cy.contains('Edit account details').click()
+            cy.get('.input-group.col-md-4').should('contain', loginName)      
+
+        })
+    })
+})
