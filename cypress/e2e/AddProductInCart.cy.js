fed14015b4ea51bfc118bba6c710f7637b1ae4c8
Amit261087 <amit.sharmaamit.sharma692@gmail.com>
2023-07-13 22:34:24 +0530

    test

--- cypress/e2e/AddProductInCart.cy.js
+++ cypress/e2e/AddProductInCart.cy.js
@@ -31,10 +31,80 @@ describe('Add Product in Cart', function () {
       .and('have.text', 'Home')
       .focus()
       .click();
+      cy.get("a[class$='prdocutname']").then(($products) => {
+        const desiredProduct = 'New Ladies High Wedge Heel Toe Thong Diamante Flip Flop Sandals';
+        const indexes = [];
+      
+        $products.each((index, $el) => {
+          const text = $el.innerText;
+          if (text === desiredProduct) {
+            indexes.push(index);
+          }
+        });
+      
+        cy.get('.productcart').then(($addToCartButtons) => {
+          const adjustedIndexes = indexes.map((index) => {
+            let outOfStockCount = 0;
+            for (let i = 0; i < index; i++) {
+              const isVisible = Cypress.$($addToCartButtons[i]).find('.nostock').is(':visible');
+              if (isVisible) {
+                outOfStockCount++;
+              }
+            }
+            return index - outOfStockCount;
+          });
+      
+          adjustedIndexes.forEach((adjustedIndex) => {
+            cy.wrap($addToCartButtons[adjustedIndex]).click();
+          });
+        });
+      });
+            
+      
+      
+      
+  });
+});
+
+
+/*
+let loginName;
+let password;
+
+before(() => {
+  cy.fixture('userCredentials.json').then((credential) => {
+    loginName = Cypress.env('loginName', credential.loginName);
+    password = Cypress.env('password', credential.password);
+  });
+});
+
+describe('Add Product in Cart', function () {
+  it('Add Product in Cart', () => {
+    cy.visit('https://automationteststore.com');
+    cy.contains('Login or register')
+      .should('be.visible')
+      .click();
+    cy.get('#loginFrm_loginname')
+      .should('be.visible')
+      .type(loginName);
+    cy.get('#loginFrm_password')
+      .should('be.visible')
+      .type(password);
+    cy.get("button[title='Login']")
+      .should('be.visible')
+      .click();
+    cy.get('.maintext')
+      .should('be.visible')
+      .and('contain', 'My Account');
+    cy.get('.active.menu_home')
+      .should('be.visible')
+      .and('have.text', 'Home')
+      .focus()
+      .click();
 
     cy.get("a[class$='prdocutname']").each(($el, index, $list) => {
       const text = $el.text();
-      if (text === 'New Ladies High Wedge Heel Toe Thong Diamante Flip Flop Sandals') {
+      if (text === 'Skinsheen Bronzer Stick') {
         cy.get('.nostock')
           .its('length')
           .then((count) => {
@@ -49,3 +119,4 @@ describe('Add Product in Cart', function () {
     });
   });
 });
+*/
