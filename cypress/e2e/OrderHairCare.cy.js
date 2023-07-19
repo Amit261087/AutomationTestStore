6e5717390c6e146e85ec7c40a210276b4fc1f051
Amit261087 <amit.sharmaamit.sharma692@gmail.com>
2023-07-19 19:54:14 +0530

    test

--- cypress/e2e/OrderHairCare.cy.js
+++ cypress/e2e/OrderHairCare.cy.js
@@ -1,19 +1,29 @@
+import testdata from '../fixtures/Data.json'
 let loginName;
 let password;
 
-before(()=>{
-    cy.fixture('userCredentials.json').then((credential)=>{
+before(() => {
+    cy.fixture('userCredentials.json').then((credential) => {
         loginName = credential.loginName;
         password = credential.password;
     })
 })
 
-describe('Order Hair Care Products', function(){
-    it('Order Hair Care Products', function(){
+describe('Order Hair Care Products', function () {
+    it('Order Hair Care Products', function () {
         cy.visit('https://automationteststore.com/')
         cy.contains('Login or register').should('be.visible').click();
         cy.get('#loginFrm_loginname').should('be.visible').type(loginName);
         cy.get('#loginFrm_password').should('be.visible').type(password);
         cy.get("button[title='Login']").should('be.visible').click();
+        cy.get('.nav-pills.categorymenu').contains('Hair Care').click()
+        testdata.productName.forEach((element) => {
+            cy.get(".fixed_wrapper .prdocutname").each(($el, index, $list) => {
+                if ($el.text() === element) {
+                    cy.log($el.text())
+                    cy.get('.productcart').eq(index).click();
+                }
+            });
+        })
     })
 })
