ea1814aad79f93225899a7781b8f7da0c287a618
Amit261087 <amit.sharmaamit.sharma692@gmail.com>
2023-07-22 18:19:27 +0530

    test

--- cypress/e2e/OrderMakeUp.cy.js
+++ cypress/e2e/OrderMakeUp.cy.js
@@ -9,15 +9,15 @@ before(() => {
     })
 })
 
-describe('Order Hair Care Products', function () {
-    it('Order Hair Care Products', function () {
+describe('Order Makeup Products', function () {
+    it('Order Makeup Products', function () {
         cy.visit('https://automationteststore.com/')
         cy.contains('Login or register').should('be.visible').click();
         cy.get('#loginFrm_loginname').should('be.visible').type(loginName);
         cy.get('#loginFrm_password').should('be.visible').type(password);
         cy.get("button[title='Login']").should('be.visible').click();
-        cy.get('.nav-pills.categorymenu').contains('Hair Care').click()
-        testdata.hairProductName.forEach((element) => {
+        cy.get('.nav-pills.categorymenu').contains('Makeup').click()
+        testdata.makeupProductName.forEach((element) => {
             cy.get('.col-md-3.col-sm-6.col-xs-12').then(() => {
                 cy.contains('.prdocutname', element)
                     .parents('.col-md-3.col-sm-6.col-xs-12')
--- cypress/e2e/OrderMenProducts.cy.js
+++ cypress/e2e/OrderMenProducts.cy.js
@@ -9,15 +9,15 @@ before(() => {
     })
 })
 
-describe('Order Hair Care Products', function () {
-    it('Order Hair Care Products', function () {
+describe('Order Men Products', function () {
+    it('Order Men Products', function () {
         cy.visit('https://automationteststore.com/')
         cy.contains('Login or register').should('be.visible').click();
         cy.get('#loginFrm_loginname').should('be.visible').type(loginName);
         cy.get('#loginFrm_password').should('be.visible').type(password);
         cy.get("button[title='Login']").should('be.visible').click();
-        cy.get('.nav-pills.categorymenu').contains('Hair Care').click()
-        testdata.hairProductName.forEach((element) => {
+        cy.get('.nav-pills.categorymenu li:nth-child(6)').contains('Men').click({force:true})
+        testdata.menProductName.forEach((element) => {
             cy.get('.col-md-3.col-sm-6.col-xs-12').then(() => {
                 cy.contains('.prdocutname', element)
                     .parents('.col-md-3.col-sm-6.col-xs-12')
